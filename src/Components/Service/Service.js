72e8f896065f2074661e9fe4b6e8f93c1f1f9792
Sadnan-2019 <sadnan.wow@gmail.com>
2023-05-10 17:22:54 +0600

    landing   is ongoing

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -5,16 +5,20 @@ import "./Service.css"
 const Service = () => {
     return (
         <div>
-            <div class=" ">
-  <div class="shape            "></div>
+            <div class=" "data-aos="fade-right"
+                    data-aos-duration="1000">
+  <div class="shape            "  ></div>
   
    
 </div>
-            <div class="shapetwo">
-  {/* <div class="shapetwo"></div> */}
+           <div className='' data-aos="fade-left"
+                    data-aos-duration="1000">
+           <div class="shapetwo">
+   
  
    
-</div>
+   </div>
+           </div>
 
         </div>
     );
