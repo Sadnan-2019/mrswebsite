35d9da6e6932c78c4548c418240d24ab7a1471d3
Sadnan-2019 <sadnan.wow@gmail.com>
2023-05-16 13:16:09 +0600

    landing   is ongoing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -151,7 +151,7 @@ const Landing = () => {
                   {/* <div class="shape"></div> */}
                 </div>
 
-                <div class="  sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0  ">
+                <div class="  sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0 rotation-container ">
                   <img src={camera} alt="" className="rounded-full h-5/6 w-5/6 " />
                 </div>
               
@@ -178,82 +178,233 @@ const Landing = () => {
             </section>
           </div>
           <div className=" ">
-            <div className="hero      sm:min-h-screen-0 lg:min-h-screen landingtwo">
-              <div className="  sm:bg-opacity-60"></div>
-              <div className="hero-content text-center text-neutral-content lg:text-neutral-content">
-                <div className="max-w-md">
-                  <h1
-                    className="lg:mb-5 sm:text-2xl lg:text-4xl font-bold"
-                    data-aos="fade-right"
-                    data-aos-offset="300"
-                    data-aos-delay="5000"
-                    data-aos-duration="3000"
+            <section className="landingone  blog h-auto landingone     bg-gradient-to-r from-[#92468E] to-[#38235D] shadow-xl  text-white">
+              <div className=" md:py-20 py-10 lg:py-24 lg:mx-16 mx-8 grid grid-cols-1 lg:grid-cols-2  gap-5 justify-center items-center">
+                <div className="sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0">
+                  {/* <div className="hidden lg:flex items-center gap-2   lg:bg-gray-100 w-fit px-3 rounded-full py-2 border-[1px]  lg:border-gray-200 mb-5">
+            <p className="hidden lg:block px-2 py-[2px] text-white lg:rounded-full  " style={{backgroundColor: "#453364" }}>
+              $
+            </p>
+            <p className='hidden lg:block text-sm '>Discover the easiest way to get our service!</p>
+          </div> */}
+                  <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    className="md:text-[43px] font-bold text-[20px] text-left poppins-b "
+                    style={{ lineHeight: "120%", color: "white" }}
                   >
-                    Welcome to The{" "}
-                  </h1>
-                  <h1
-                    className="lg:mb-5 sm:text-2xl lg:text-4xl font-bold"
-                    data-aos="fade-right"
-                    data-aos-offset="300"
-                    data-aos-delay="5000"
-                    data-aos-duration="3000"
-                  >
-                    Passive Exercise
-                  </h1>
-                  <p className="lg:mb-5 animate__animated animate__fadeInRight">
-                    Some Physical Conditions Need to be Treated by Passive
-                    Exercise. Stroke Induced Paralysis is One of Them.
+                    Media Resource Solution
                   </p>
+
+                  {/* <p
+                  
+                    data-aos="fade-up"
+                    data-aos-duration="2000"
+                    className="max-w-lg mt-7 font-bold   md:text-[25px] poppins-t    "
+                    style={{ lineHeight: "120%", color: "white" }}
+                  >
+                    First ever one-stop high-quality media resource solution at
+                    your doorstep.{" "}
+                  </p> */}
+
+                  <h1
+                    className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t "
+                    style={{ lineHeight: "120%", color: "white" }}
+                  >
+                    First ever one-stop high-quality{" "}
+                    <span className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t   ">
+                      {/* Style will be inherited from the parent element */}
+                      <Typewriter
+                        words={["  media resource solution at your doorstep"]}
+                        loop={true}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        cursorBlinking={true}
+                        onLoopDone={handleDone}
+                        onType={handleType}
+                      />
+                    </span>
+                  </h1>
+
+                  <div className="flex items-center mt-2 font-bold">
+                    <FaPhoneVolume
+                      className="lg:text-2xl text-[20px] "
+                      style={{ color: "white" }}
+                    />
+                    <span
+                      className="lg:text-2xl text-[20px] "
+                      style={{ color: "white" }}
+                    >
+                      {" "}
+                      Call Now: 01619848555
+                    </span>
+                  </div>
+
+                  <div
+                    data-aos="fade-up"
+                    data-aos-duration="3000"
+                    className="flex  gap-4 mt-10 text-center"
+                  >
+                    <a
+                      className=" hidden hover-button bg-gradient-to-r from-[#92468E] to-[#38235D]  md:inline-flex items-center     border          text-white "
+                      href="/"
+                    >
+                      <span className="text-sm font-medium">
+                        {" "}
+                        GET STARTED WITH US
+                      </span>
+                      <BsArrowRight className="text-lg ml-2" />
+                    </a>
+                    {/* <button class="hover-button">Hover Me</button> */}
+                  </div>
+
+                  {/* <div class="shape"></div> */}
                 </div>
+
+                <div class="  sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0 rotation-container ">
+                  <img src={camera} alt="" className="rounded-full h-5/6 w-5/6 " />
+                </div>
+              
+
               </div>
-            </div>
+
+              <div
+                data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1500"
+              >
+                <svg
+                  className=""
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1440 320"
+                >
+                  <path
+                    fill="white"
+                    fill-opacity="50"
+                    d="M0,128L120,160C240,192,480,256,720,272C960,288,1200,256,1320,240L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+                  ></path>
+                </svg>
+              </div>
+            </section>
           </div>
           <div className=" ">
-            <div className="hero      sm:min-h-screen-0 lg:min-h-screen landingthree">
-              <div className="  bg-opacity-60"></div>
-              <div className="hero-content text-center text-neutral-content lg:text-neutral-content">
-                <div className="max-w-md">
-                  <h1
-                    className="lg:mb-5 sm:text-2xl lg:text-4xl font-bold"
-                    data-aos="fade-right"
-                    data-aos-offset="300"
-                    data-aos-delay="5000"
-                    data-aos-duration="3000"
+            <section className="landingone  blog h-auto landingone     bg-gradient-to-r from-[#92468E] to-[#38235D] shadow-xl  text-white">
+              <div className=" md:py-20 py-10 lg:py-24 lg:mx-16 mx-8 grid grid-cols-1 lg:grid-cols-2  gap-5 justify-center items-center">
+                <div className="sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0">
+                  {/* <div className="hidden lg:flex items-center gap-2   lg:bg-gray-100 w-fit px-3 rounded-full py-2 border-[1px]  lg:border-gray-200 mb-5">
+            <p className="hidden lg:block px-2 py-[2px] text-white lg:rounded-full  " style={{backgroundColor: "#453364" }}>
+              $
+            </p>
+            <p className='hidden lg:block text-sm '>Discover the easiest way to get our service!</p>
+          </div> */}
+                  <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    className="md:text-[43px] font-bold text-[20px] text-left poppins-b "
+                    style={{ lineHeight: "120%", color: "white" }}
                   >
-                    Shortwave Diathermy
-                  </h1>
-                  <p className="lg:mb-5 animate__animated animate__fadeInRight">
-                    This Machine is Used to Give Deep Heat to the inflamed
-                    Tissues of the Body. It Improves Circulation thus Enhances
-                    Healing Process.
+                    Media Resource Solution
                   </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className=" ">
-            <div className="hero      sm:min-h-screen-0 lg:min-h-screen landingfour">
-              <div className="  sm:bg-opacity-60"></div>
-              <div className="hero-content text-center text-neutral-content lg:text-neutral-content">
-                <div className="max-w-md">
-                  <h1
-                    className="lg:mb-5 sm:text-2xl lg:text-4xl font-bold"
-                    data-aos="fade-right"
-                    data-aos-offset="300"
-                    data-aos-delay="5000"
-                    data-aos-duration="3000"
+
+                  {/* <p
+                  
+                    data-aos="fade-up"
+                    data-aos-duration="2000"
+                    className="max-w-lg mt-7 font-bold   md:text-[25px] poppins-t    "
+                    style={{ lineHeight: "120%", color: "white" }}
                   >
-                    Active Exercise
+                    First ever one-stop high-quality media resource solution at
+                    your doorstep.{" "}
+                  </p> */}
+
+                  <h1
+                    className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t "
+                    style={{ lineHeight: "120%", color: "white" }}
+                  >
+                    First ever one-stop high-quality{" "}
+                    <span className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t   ">
+                      {/* Style will be inherited from the parent element */}
+                      <Typewriter
+                        words={["  media resource solution at your doorstep"]}
+                        loop={true}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        cursorBlinking={true}
+                        onLoopDone={handleDone}
+                        onType={handleType}
+                      />
+                    </span>
                   </h1>
-                  <p className="lg:mb-5 animate__animated animate__fadeInRight">
-                    Active Exercise Improves Function, Increases Capability &
-                    Boosts Up. Physical & Mental Wellbeing. It can reduce the
-                    risk of many diseases.
-                  </p>
+
+                  <div className="flex items-center mt-2 font-bold">
+                    <FaPhoneVolume
+                      className="lg:text-2xl text-[20px] "
+                      style={{ color: "white" }}
+                    />
+                    <span
+                      className="lg:text-2xl text-[20px] "
+                      style={{ color: "white" }}
+                    >
+                      {" "}
+                      Call Now: 01619848555
+                    </span>
+                  </div>
+
+                  <div
+                    data-aos="fade-up"
+                    data-aos-duration="3000"
+                    className="flex  gap-4 mt-10 text-center"
+                  >
+                    <a
+                      className=" hidden hover-button bg-gradient-to-r from-[#92468E] to-[#38235D]  md:inline-flex items-center     border          text-white "
+                      href="/"
+                    >
+                      <span className="text-sm font-medium">
+                        {" "}
+                        GET STARTED WITH US
+                      </span>
+                      <BsArrowRight className="text-lg ml-2" />
+                    </a>
+                    {/* <button class="hover-button">Hover Me</button> */}
+                  </div>
+
+                  {/* <div class="shape"></div> */}
                 </div>
+
+                <div class="  sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0 rotation-container ">
+                  <img src={camera} alt="" className="rounded-full h-5/6 w-5/6 " />
+                </div>
+              
+
               </div>
-            </div>
+
+              <div
+                data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1500"
+              >
+                <svg
+                  className=""
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1440 320"
+                >
+                  <path
+                    fill="white"
+                    fill-opacity="50"
+                    d="M0,128L120,160C240,192,480,256,720,272C960,288,1200,256,1320,240L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+                  ></path>
+                </svg>
+              </div>
+            </section>
           </div>
+           
+         
         </Slider>
       </div>
     </div>
