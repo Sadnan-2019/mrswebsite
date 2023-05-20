02e61a2bb7791be6dc2efdae8e767919054c08f1
Sadnan-2019 <sadnan.wow@gmail.com>
2023-05-20 15:29:32 +0600

    landing   is ongoing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -24,7 +24,7 @@ const Landing = () => {
     afterChange: (index) => {
       setCurrentSlide(index);
     },
-    // autoplay:"true",
+    autoplay:"true",
     autoplaySpeed: 5000,
     responsive: [
       {
