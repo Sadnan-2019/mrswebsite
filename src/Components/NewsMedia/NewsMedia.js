35fce0ca8068ee718b89653912f6be4c61ba8b54
Sadnan-2019 <sadnan.wow@gmail.com>
2023-05-24 16:31:31 +0600

    landing   is ongoing

--- src/Components/NewsMedia/NewsMedia.js
+++ src/Components/NewsMedia/NewsMedia.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
+import  mediaone from "../../assets/MEDIAONE.jpeg"
+import  mediatwo from "../../assets/MEDIATWO.jpg"
+import  mediathree from "../../assets/MEDIATHREE.jpg"
 
 const NewsMedia = () => {
     return (
         <div>
             <Nav className=" "></Nav>
-            <div className=''>
+           
 
-                
-            </div>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-2 px-4 mx-4  items-center justify-items-center">
+  <div>
+    <img src={mediaone} alt="First Picture" class="w-full p-3" />
+  </div>
+  <div class="p-2">
+    <div className='flex gap-3  items-center justify-items-center'>
+    <div>
+      <img src={mediaone} alt="Second Picture" class="w-full p-1"  />
+    </div>
+    <div>
+      <img src={mediaone} alt="Third Picture" class="w-full p-1" />
+    </div>
+    </div>
+    <div className='flex gap-3   items-center justify-items-center'>
+    <div>
+      <img src={mediaone} alt="Second Picture" class="w-full p-1"/>
+    </div>
+    <div>
+      <img src={mediaone} alt="Third Picture" class="w-full p-1" />
+    </div>
+    </div>
+     
+  </div>
+</div>
+
         </div>
     );
 };
