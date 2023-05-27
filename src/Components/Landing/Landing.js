9f1a216368f3f4dd12feb7bd3446f5e65ad58864
Sadnan-2019 <sadnan.wow@gmail.com>
2023-05-27 16:40:49 +0600

    landing   is ongoing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -259,6 +259,99 @@ const Landing = () => {
               </div>
             </section>
           </div>
+          <div className=" ">
+            <section className="landingthree  blog h-auto       shadow-xl  text-white">
+              <div className=" md:py-20 py-10 lg:py-24 lg:mx-16 mx-8 grid grid-cols-1 lg:grid-cols-2  gap-5 justify-center items-center">
+                <div className="sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0">
+                  <h2
+                    style={{ lineHeight: "120%", color: "white" }}
+                    className={`slide-text  md:text-[43px] font-bold text-[20px] text-left  ${
+                      currentSlide === 2 ? "animated" : ""
+                    }`}
+                  >
+                    {" "}
+                    Media Resource Solution Thired
+                  </h2>
+
+                  <h1
+                    className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t "
+                    style={{ lineHeight: "120%", color: "white" }}
+                  >
+                    First ever one-stop high-quality media resource solution at your doorstep{" "}
+                    {/* <span className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t   ">
+                      <Typewriter
+                        words={["  media resource solution at your doorstep"]}
+                        loop={true}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        cursorBlinking={true}
+                        onLoopDone={handleDone}
+                        onType={handleType}
+                      />
+                    </span> */}
+                  </h1>
+
+                  <div className="flex items-center mt-2 font-bold">
+                    <FaPhoneVolume
+                      className="lg:text-2xl text-[20px] "
+                      style={{ color: "white" }}
+                    />
+                    <span
+                      className="lg:text-2xl text-[20px] "
+                      style={{ color: "white" }}
+                    >
+                      {" "}
+                      Call Now: 01619848555
+                    </span>
+                  </div>
+
+                  <div className="flex  gap-4 mt-10 text-center">
+                    <a
+                      href="/"
+                      className={`slide-text   hidden hover-button bg-gradient-to-r from-[#92468E] to-[#38235D]  md:inline-flex items-center     border          text-white   ${
+                        currentSlide === 2 ? "animated" : ""
+                      }`}
+                    >
+                      <span className="text-sm font-medium">
+                        {" "}
+                        GET STARTED WITH US
+                      </span>
+                      <BsArrowRight className="text-lg ml-2" />
+                    </a>
+                  </div>
+                </div>
+
+                {/* <div class="  sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0  ">
+                  <img
+                    src={camera}
+                    alt=""
+                    className="rounded-full h-5/6 w-5/6 "
+                  />
+                </div> */}
+              </div>
+
+              <div
+                data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1500"
+              >
+                <svg
+                  className=""
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1440 320"
+                >
+                  <path
+                    fill="white"
+                    fill-opacity="50"
+                    d="M0,128L120,160C240,192,480,256,720,272C960,288,1200,256,1320,240L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+                  ></path>
+                </svg>
+              </div>
+            </section>
+          </div>
           
         </Slider>
       </div>
